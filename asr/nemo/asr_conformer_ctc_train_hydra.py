0cd226884a7c538c6e7d4bdf9a8e7b2fc111a674
J.P Lee <koreanfeel@gmail.com>
2022-10-17 23:43:43 +0900

    NeMo Conformer-CTC 코드 추가

---
+++ asr/nemo/asr_conformer_ctc_train_hydra.py
@@ -0,0 +1,52 @@
+# This is where the an4/ directory will be placed.
+# Change this if you don't want the data to be extracted in the current directory.
+
+# NeMo's "core" package
+import logging
+
+from nemo.utils.exp_manager import exp_manager
+from pytorch_lightning.plugins import DDPPlugin
+
+import nemo
+# NeMo's ASR collection - this collections contains complete ASR models and
+# building blocks (modules) for ASR
+import nemo.collections.asr as nemo_asr
+
+from nemo.core.config import hydra_runner
+from omegaconf import OmegaConf, open_dict
+
+@hydra_runner(config_path="./conf/conformer/", config_name="conformer_ctc_bpe")
+def main(cfg):
+    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
+
+    cfg.model.tokenizer.dir = "scripts/tokenizers/ksponspeech/tokenizer_spe_unigram_v5000/"  # note this is a directory, not a path to a vocabulary file
+    cfg.model.tokenizer.type = "bpe"
+
+    import pytorch_lightning as pl
+    # trainer = pl.Trainer(strategy=DDPPlugin(find_unused_parameters=False),
+    #                      max_epochs=50,
+    #                      log_every_n_steps=100
+    #                      )
+    trainer = pl.Trainer(**cfg.trainer)
+    exp_manager(trainer, cfg.get("exp_manager", None))
+
+    train_manifest = 'scripts/ksponspeech/train_manifest.json'
+    dev_manifest = 'scripts/ksponspeech/dev_manifest.json'
+
+    # Update paths to dataset
+    cfg.model.train_ds.manifest_filepath = train_manifest
+    cfg.model.validation_ds.manifest_filepath = dev_manifest
+
+    first_asr_model = nemo_asr.models.EncDecCTCModelBPE(cfg=cfg.model, trainer=trainer)
+
+    from pytorch_lightning.utilities.model_summary import summarize
+    summarize(first_asr_model)
+
+    # Start training!!!
+    trainer.fit(first_asr_model)
+
+    first_asr_model.save_to("conformer_ctc_bpe_model.nemo")
+
+
+if __name__ == '__main__':
+    main()
