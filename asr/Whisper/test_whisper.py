faec4b7c125df3c1650b88980fc60ed2fcf4fbd7
JH-debug <oreomasida@gmail.com>
2022-10-18 21:25:07 +0900

    test_whisper 수정

--- asr/Whisper/test_whisper.py
+++ asr/Whisper/test_whisper.py
@@ -1,5 +1,6 @@
 import whisper
 import torch
+import os
 import json
 from tqdm import tqdm
 import numpy as np
@@ -17,6 +18,7 @@ def parse_args():
     parser.add_argument('--dev',  default=False,  action='store_true')
     parser.add_argument('--eval_clean',  default=False, action='store_true')
     parser.add_argument('--eval_other', default=False, action='store_true')
+    parser.add_argument('--cuda', default='cuda:1', type=str)
     args = parser.parse_args()
 
     return args
@@ -32,6 +34,41 @@ def read_manifest(path):
     return manifest
 
 
+def inference(manifest_path, model, transcribe_options):
+    manifest_data = read_manifest(manifest_path)
+    file_name = manifest_path.split('/')[-1].split('.')[0]
+
+    audio_filepaths = []
+    references = []
+    transcriptions = []
+
+    for i in tqdm(manifest_data, desc='Inference'):
+        audio_filepath = i['audio_filepath']
+        # audio = torch.from_numpy(wavfile.read(audio_filepath)[1].copy())
+        text = i['text']
+
+        audio = whisper.load_audio(audio_filepath)
+        audio = whisper.pad_or_trim(audio)
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        hypothesis = model.decode(mel, transcribe_options)
+
+        audio_filepaths.append(audio_filepath)
+        transcriptions.append(hypothesis.text)
+        references.append(text)
+
+    data = pd.DataFrame(dict(audio_filepaeh=audio_filepaths, reference=references, hypothesis=transcriptions))
+    data.to_csv(f'result/{file_name}_result.csv', index=False, encoding="utf-8-sig")
+
+    normalizer = BasicTextNormalizer()
+    data["hypothesis_clean"] = [normalizer(text) for text in data["hypothesis"]]
+    data["reference_clean"] = [normalizer(text) for text in data["reference"]]
+    data.to_csv(f'result/{file_name}_result.csv', index=False, encoding="utf-8-sig")
+
+    # wer = jiwer.wer(list(data["reference_clean"]), list(data["hypothesis_clean"]))
+    # print(f"WER: {wer * 100:.2f} %")
+
+
 def main():
     args = parse_args()
 
@@ -39,61 +76,34 @@ def main():
         print('Please select the data you want to inference')
 
     else:
-        if args.train:
-            manifest_path = '../nemo/manifests/train.json'
-
-        elif args.dev:
-            manifest_path = '../nemo/manifests/dev.json'
-
-        elif args.eval_clean:
-            manifest_path = '../nemo/manifests/eval_clean.json'
-
-        else:
-            manifest_path = '../nemo/manifests/eval_other.json'
-
-        model = whisper.load_model("base", device="cuda:1")
-
+        print("Loading model...")
+        model = whisper.load_model("base", device=args.cuda)
         print(
             f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
             f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
         )
 
-        manifest_data = read_manifest(manifest_path)
-        file_name = manifest_path.split('/')[-1].split('.')[0]
-        print(file_name)
-
         options = dict(language="Korean", beam_size=5)
         transcribe_options = whisper.DecodingOptions(fp16=False, task='transcribe', without_timestamps=True, **options)
 
-        audio_filepaths = []
-        references = []
-        transcriptions = []
+        if not os.path.isdir('result'):
+            os.mkdir('result')
 
-        for i in tqdm(manifest_data):
-            audio_filepath = i['audio_filepath']
-            # audio = torch.from_numpy(wavfile.read(audio_filepath)[1].copy())
-            text = i['text']
+        if args.train:
+            manifest_path = '../nemo/manifests/train.json'
+            inference(manifest_path, model, transcribe_options)
 
-            audio = whisper.load_audio(audio_filepath)
-            audio = whisper.pad_or_trim(audio)
-            mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        elif args.dev:
+            manifest_path = '../nemo/manifests/dev.json'
+            inference(manifest_path, model, transcribe_options)
 
-            hypothesis = model.decode(mel, transcribe_options)
+        elif args.eval_clean:
+            manifest_path = '../nemo/manifests/eval_clean.json'
+            inference(manifest_path, model, transcribe_options)
 
-            audio_filepaths.append(audio_filepath)
-            transcriptions.append(hypothesis.text)
-            references.append(text)
-
-        data = pd.DataFrame(dict(audio_filepaeh=audio_filepaths, reference=references, hypothesis=transcriptions))
-        data.to_csv(f'result/{file_name}_result.csv', index=False)
-
-        normalizer = BasicTextNormalizer()
-        data["hypothesis_clean"] = [normalizer(text) for text in data["hypothesis"]]
-        data["reference_clean"] = [normalizer(text) for text in data["reference"]]
-        data.to_csv(f'result/{file_name}_result.csv', index=False)
-
-        wer = jiwer.wer(list(data["reference_clean"]), list(data["hypothesis_clean"]))
-        print(f"WER: {wer * 100:.2f} %")
+        else:
+            manifest_path = '../nemo/manifests/eval_other.json'
+            inference(manifest_path, model, transcribe_options)
 
 
 if __name__ == '__main__':
