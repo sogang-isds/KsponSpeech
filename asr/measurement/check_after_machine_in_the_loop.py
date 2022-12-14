8ea0a0549e77838684291a9a03c6772cd5832106
J.P Lee <koreanfeel@gmail.com>
2022-12-14 17:25:26 +0900

    에러 후보 가져오는 부분을 별도 함수로 분리

--- asr/measurement/check_after_machine_in_the_loop.py
+++ asr/measurement/check_after_machine_in_the_loop.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 import csv
 
+from asr.measurement.analyze import get_annotate_error_candidates
+
 
 def check_after_machine_in_the_loop():
     final_reference = []
@@ -15,40 +17,15 @@ def check_after_machine_in_the_loop():
     for d in data:
         final_reference.append(d['meta'][0]['text'])
 
-    text_clean = pd.read_csv('../../eval_clean_add_metrics.csv')
+    metric_result_file = '../../eval_clean_add_metrics.csv'
+    # metric_result_file = '../../eval_clean_add_metrics_squeezeformer.csv'
+    text_clean = pd.read_csv(metric_result_file)
 
     for file, origin, ref in zip(text_clean['audio_filepath'], text_clean['reference'], final_reference):
         if origin != ref:
             human_craft_file.append(os.path.basename(file))
 
-    with open('../../eval_clean_add_metrics.csv', 'r', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        header = next(reader)
-
-        max_len = 116
-        count = 0
-
-        for i, elem in enumerate(reader):
-            audio_filepath = elem[0]
-            reference = elem[1]
-            hypothesis = elem[2]
-            try:
-                ref_ppl = float(elem[3])
-                hyp_ppl = float(elem[4])
-            except ValueError:
-                continue
-
-            cer = float(elem[5])
-            hyp_len = len(hypothesis)
-
-            normalized_cer = cer * hyp_len / max_len
-
-            if ref_ppl > hyp_ppl:
-                diff_ratio_a = (ref_ppl - hyp_ppl) / ref_ppl
-
-                if diff_ratio_a > 0.15 and normalized_cer > 0.02:
-                    machine_craft_file.append(os.path.basename(audio_filepath))
-                    count += 1
+    machine_craft_file = get_annotate_error_candidates(metric_result_file)
 
     final_file = []
 
@@ -59,6 +36,7 @@ def check_after_machine_in_the_loop():
             final_file.append(origin_ref)
 
     human_machine_intersection = set(human_craft_file).intersection(set(machine_craft_file))
+    print('\n===== Experiment Result =====')
     print('Human_machine_intersection: ', len(human_machine_intersection))
     print('Human in the loop: ', len(human_craft_file))
     print('Machine in the loop: ', len(machine_craft_file))
